b9ec771e91c7827e24b148db1c411eec17b6323b
lu <48860067@est.ort.edu.ar>
2025-05-04 19:12:09 -0300

    commit
    Hicimos el controller y arrancamos con la view
    
    Co-Authored-By: ramaa <207027482+RamiroPita11@users.noreply.github.com>

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -13,8 +13,56 @@ public class HomeController : Controller
         _logger = logger;
     }
 
-    public IActionResult Index()
+     public IActionResult Index()
     {
+        Discos.InicializarDiscos();
+        ViewBag.discos = Discos.devolverDiccionario();
         return View();
     }
+    public IActionResult SelectDisco(int id)
+    {
+        ViewBag.discos = Discos.devolverDiccionario();
+        if (ViewBag.discos.ContainsKey(id))
+        {
+            ViewBag.infoDisco = Discos.devolverDiccionario()[id];
+            ViewBag.ID = id;
+        }
+        else
+        {
+            ViewBag.Error = "Error, el disco no existe";
+        }
+
+        return View("infoDisco");
+    }
+    public IActionResult MostarArtista(int dni)
+    {
+        ViewBag.Disco = Discos.devolverDiccionario();
+        if (ViewBag.Disco.ContainsKey(dni))
+        {
+            ViewBag.infoArtista = Discos.devolverDiccionario()[dni].artista;
+            ViewBag.DNI = dni;
+        }
+        else
+        {
+            ViewBag.Error = "Error, el artista no existe";
+        }
+
+        return View("infoArtista");
+    }
+    public IActionResult MostrarProductor(int dni)
+    {
+        ViewBag.Disco = Discos.devolverDiccionario();
+        if (ViewBag.integrantes.ContainsKey(dni))
+        {
+            ViewBag.infoProductor = Discos.devolverDiccionario()[dni].productor;
+            ViewBag.DNI = dni;
+        }
+        else
+        {
+            ViewBag.Error = "Error, el productor no existe";
+        }
+
+        return View("infoProductor");
+    }
+    
 }
